6f8a8ec46e0514ce68f3ee2ed4e983e63d4b6ed2
Nathan Wood <nathan.ronald.wood@gmail.com>
2022-05-18 09:33:34 -0500

    Solution successful

--- stringDecoder.js
+++ stringDecoder.js
@@ -1,7 +1,13 @@
-const decoder = (code) => {};
-
-
-
-
+const decoder = (code) => {
+  let decoded = [];
+  for (let i = 0; i < code.length; i += Number(code[i]) + 2) {
+    decoded.push(code[Number(code[i]) + i + 1]);
+  }
+  return decoded.join("");
+};
 
 console.log(decoder("0h2xce5ngbrdy"), "should be `hey`");
+console.log(decoder("3vdfn"), "should be `n`");
+console.log(decoder("0r"), "should be `r`");
+console.log(decoder("2bna0p1mp2osl0e"), "should be `apple`");
+console.log(decoder("0y4akjfe0s"), "should be `yes`");
